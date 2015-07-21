732be2fd8e26ae340d6c4b6815ef45fadcb91479
ImMrz <442693420@qq.com>
2015-07-21 23:22:14 +0800

    添加消息界面

---
+++ CZC/Views/DiscountPromotionTableViewCell.h
@@ -0,0 +1,13 @@
+//
+//  DiscountPromotionTableViewCell.h
+//  CZC
+//
+//  Created by 张浩 on 15/7/21.
+//  Copyright (c) 2015年 周德艺. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DiscountPromotionTableViewCell : UITableViewCell
+
+@end
